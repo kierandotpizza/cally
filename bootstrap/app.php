026c6d4e8d9045654f4bd1fded012ba96c632d55
Kieran Heron <hello@kieran.pro>
2018-03-02 19:07:35 +0000

    Added a little base_path helper :surfer:

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,18 +8,18 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 /** Attempt to load the .env file through Dotenv */
 try {
-    $dotenv = (new Dotenv\Dotenv(__DIR__ . '/../'))->load();
+    $dotenv = (new Dotenv\Dotenv(base_path()))->load();
 } catch (Dotenv\Exception\InvalidPathException $e) {
     //
 }
 
 /** Require our container */
-require_once __DIR__ . '/container.php';
+require_once  base_path('/bootstrap/container.php');
 
 /** Get our route collection */
 $route = $container->get(League\Route\RouteCollection::class);
 
-require_once __DIR__ . '/../routes/web.php';
+require_once base_path('routes/web.php');
 
 /** Dispatch the request and response */
 $response = $route->dispatch(
